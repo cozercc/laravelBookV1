4d057c27824074549be10f55a60107650b9565b1
cozer <cozercn@gmail.com>
2022-09-27 14:58:41 +0900

    user登録周りの作業

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -3,10 +3,33 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\User;
 
 class UsersController extends Controller
 {
     public function create(){
         return view('users.create');
+    }
+
+    public function show(User $user){
+        return view('users.show', compact('user'));
+    }
+
+    public function store(Request $request){
+        $this->validate($request, [
+            'name' => 'required|unique:users|max:50',
+            'email' => 'required|email|unique:users|max:255',
+            'password' => 'required|confirmed|min:6'
+        ]);
+
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => bcrypt($request->password),
+        ]);
+
+        // add flash info
+        session()->flash('success', "Welcome to my site!");
+        return redirect()->route('users.show', [$user]);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -19,3 +19,4 @@ Route::get('/about', 'StaticPagesController@about')->name('about');
 
 // ログイン
 Route::get('signup', 'UsersController@create')->name('signup');
+Route::resource('users', 'UsersController');
