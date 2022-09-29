bf2541ba679cfda9c0a353576602082a0132105a
cozer <cozercn@gmail.com>
2022-09-29 10:49:56 +0900

    ユーザー資料編集機能

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -16,6 +16,7 @@ class UsersController extends Controller
         return view('users.show', compact('user'));
     }
 
+    // 注册用户
     public function store(Request $request){
         $this->validate($request, [
             'name' => 'required|unique:users|max:50',
@@ -34,5 +35,28 @@ class UsersController extends Controller
         // add flash info
         session()->flash('success', "Welcome to my site!");
         return redirect()->route('users.show', [$user]);
+    }
+
+    public function edit(User $user){
+        return view('users.edit', compact('user'));
+    }
+
+    // 更新用户资料
+    public function update(User $user, Request $request){
+        $this->validate($request, [
+            'name' => 'required|max:50',
+            'password' => 'nullable|confirmed|min:6'
+        ]);
+
+        $data = [];
+        $data['name'] = $request->name;
+        if ($request->password){
+            $data['password'] = bcrypt($request->password);
+        }
+        $user->update($data);
+
+        session()->flash('success', '个人资料更新成功！');
+
+        return redirect()->route('users.show', $user->id);
     }
 }
