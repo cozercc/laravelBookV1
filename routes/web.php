270de4afc8c0c7d2d7dc7ad543ce3334e1a5e89f
cozer <cozercn@gmail.com>
2022-10-03 16:58:35 +0900

    一旦保存

--- routes/web.php
+++ routes/web.php
@@ -28,3 +28,10 @@ Route::delete('logout', 'SessionsController@destroy')->name('logout'); // 销毁
 
 //Mail
 Route::get('signup/confirm/{token}', 'UsersController@confirmEmail')->name('confirm_email');
+
+// 忘记密码
+Route::get('password/reset', 'PasswordController@showLinkRequestForm')->name('password.request'); // 填写 Email 的表单
+Route::post('password/email', 'PasswordController@sendResetLinkEmail')->name('password.email'); // 处理表单提交，成功的话就发送邮件，附带 Token 的链接
+
+Route::get('password/reset/{token}', 'PasswordController@showResetForm')->name('password.reset'); // 显示更新密码的表单，包含 token
+Route::post('password/reset', 'PasswordController@reset')->name('password.update'); // 对提交过来的 token 和 email 数据进行配对，正确的话更新密码
